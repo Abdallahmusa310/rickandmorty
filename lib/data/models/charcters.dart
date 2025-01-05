825551744301d9f131af25c74eed82868d9fdeb7
AbdallahAli <Abali2792004@gmail.com>
2025-01-05 20:39:19 +0200

    "create charcters model "

---
+++ lib/data/models/charcters.dart
@@ -0,0 +1,21 @@
+class Charctersmodel {
+  late int id;
+  late String name;
+  late String status;
+  late String species;
+  late String gender;
+  late String image;
+  late List<dynamic> episodes;
+  late List<dynamic> origin;
+
+  Charctersmodel.fromJson(Map<String, dynamic> json) {
+    id = json["id"];
+    name = json["name"];
+    status = json["status"];
+    species = json["species"];
+    gender = json["gender"];
+    image = json["image"];
+    episodes = json["episode"];
+    origin = json["origin"];
+  }
+}
