fbfb45416a2a7f7eb38ea2437a4411108be8df5e
AbdallahAli <Abali2792004@gmail.com>
2025-01-05 15:28:35 +0200

    "create colors file and add project colors"

---
+++ lib/constans/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const yellow = Color(0xfffdcb6e);
+const green = Color(0xff55efc4);
+const gray = Color(0xffb2bec3);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rickandmorty/router.dart';
 
 void main() {
@@ -10,9 +11,14 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key, required this.approuter});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      onGenerateRoute: approuter.generaterouter,
-      debugShowCheckedModeBanner: false,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        onGenerateRoute: approuter.generaterouter,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
